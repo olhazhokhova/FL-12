a31ec359b5b7030d153d37642266bb8764269162
olhazhokhova <zhokhova.olha@gmail.com>
2019-12-23 21:54:05 +0200

    fix

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -5,7 +5,17 @@ b = parseInt(prompt("Input triangle length side 'b'"));
 c = parseInt(prompt("Input triangle length side 'c'"));
 
 if(isNaN(a) || isNaN(b) || isNaN(c)){
-	alert('Input values should be ONLY numbers');
+	console.log('Input values should be ONLY numbers');
 } else if(a === 0 || b === 0 || c === 0){
-	alert('A triangle must have 3 sides with a positive definite length');
+	console.log('A triangle must have 3 sides with a positive definite length');
+} else if (a + b < c || b + c < a || c + a < b){
+	console.log('Triangle doesn’t exist');
+} else {
+	if(a === b && b === c){
+		console.log('Equilateral triangle');
+	} else if(a === b || b === c || c === a){
+		console.log('Isosceles triangle');
+	} else {
+		console.log('Scalene triangle');
+	}
 }
