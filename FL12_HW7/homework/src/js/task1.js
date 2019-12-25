8801b16b45ca1382b2d5f0a16f62f5c7de066b6d
olhazhokhova <zhokhova.olha@gmail.com>
2019-12-25 11:33:26 +0200

    add HW7 task1

---
+++ FL12_HW7/homework/src/js/task1.js
@@ -0,0 +1,56 @@
+let 
+	minEmailLength = 5,
+	minPassLength = 6,
+	user = 'user@gmail.com',
+	userPass = 'UserPass',
+	admin = 'admin@gmail.com',
+	adminPass = 'AdminPass';
+
+let email = prompt('Input email', '');
+
+if(!email){
+	alert('Canceled');
+} else if(email.length < minEmailLength){
+	alert('I don’t know any emails having name length less than 5 symbols');
+} else if(email === user || email === admin){
+	let pass = prompt('Input password', '');
+
+	if(!pass){
+		alert('Canceled');
+	} else if(email === user && pass === userPass || email === admin && pass === adminPass){
+
+		let changePass = confirm('Do you want to change your password?');
+
+		if(changePass){
+			pass = prompt('Input password', '');
+
+			if(!pass){
+				alert('Canceled');
+			} else if(email === user && pass === userPass || email === admin && pass === adminPass){
+				let passNew = prompt('Input new password', '');
+
+				if(!passNew){
+					alert('Canceled');
+				} else if(passNew.length < minPassLength){
+					alert('It’s too short password. Sorry.');
+				} else{
+					let passNewRepeat = prompt('Repeat new password', '');
+
+					if(passNew !== passNewRepeat){
+						alert('You wrote the wrong password.');
+					} else{
+						alert('You have successfully changed your password.');
+					}
+				}
+			} else{
+				alert('Wrong password');
+			}
+		} else{
+			alert('You have failed the change.');
+		}
+	} else{
+		alert('Wrong password');
+	}
+} else{
+	alert('I don’t know you');
+}
